e6622b8f3053a46b1c053925417f4721a286f190
rauscher-lucien <rauscher.lucien@gmail.com>
2023-03-06 18:38:53 +0100

    ADDED GRAPHING

--- image_analysis.py
+++ image_analysis.py
@@ -12,30 +12,56 @@ cov_mat = np.load(os.path.join(path, 'video_'+str(video_number)+r'_files\cov_ang
 plt.imshow(cov_mat, origin='lower')
 plt.show()
 
-# work with the eigenworms
+# work with the eigenworms ################################
 
-e_vals = np.load(os.path.join(path, 'video_' + str(video_number) + r'_files\e_vals_'+str(video_number)+'.npy'))
-eigen_worms = np.load(os.path.join(path, 'video_' + str(video_number) + r'_files\eigen_worms_'+str(video_number)+'.npy'))
+# e_vals = np.load(os.path.join(path, 'video_' + str(video_number) + r'_files\e_vals_'+str(video_number)+'.npy'))
+# eigen_worms = np.load(os.path.join(path, 'video_' + str(video_number) + r'_files\eigen_worms_'+str(video_number)+'.npy'))
+#
+# num_eig_worms = 4
+#
+# print(eigen_worms)
+#
+# fig, ax = plt.subplots()
+# ax.plot(np.cumsum(e_vals) / np.sum(e_vals), 'o', markeredgecolor=[1, 0.5, 0.1], markerfacecolor=[1, 0.5, 0.1],
+#         markersize=8)
+# ax.set_xlabel('Number of eigenworms')
+# ax.set_ylabel('Fraction of variance explained')
+# ax.set_xlim(0, num_eig_worms)
+# ax.set_ylim(0, 1)
+# ax.set_title('Variance explained by eigenworms')
+#
+# # Plot the eigenworms
+# fig, axs = plt.subplots(nrows=num_eig_worms, ncols=1, figsize=(5, 10))
+# for i in range(num_eig_worms):
+#     axs[i].plot(eigen_worms[i, :], color=[1, 0.5, 0.1], linewidth=2)
+#     # axs[i].set_xlim(0, numPixels)
+#     # axs[i].set_ylim(0, eigenWorms)
+#
+# plt.show()
 
-num_eig_worms = 4
+# plot superposition of eigenworms
 
-print(e_vals)
-print(np.cumsum(e_vals))
+num_eigenworms = 4
+fig, axs = plt.subplots(nrows=num_eigenworms, ncols=1, figsize=(5, 10))
+eigenworms_sum = np.zeros((4, 29))
+index = 1
+while os.path.exists(os.path.join(path, r'all_eigenworms\eigen_worms_'+str(index)+'.npy')):  # loop over all pics
 
-fig, ax = plt.subplots()
-ax.plot(np.cumsum(e_vals) / np.sum(e_vals), 'o', markeredgecolor=[1, 0.5, 0.1], markerfacecolor=[1, 0.5, 0.1],
-        markersize=8)
-ax.set_xlabel('Number of eigenworms')
-ax.set_ylabel('Fraction of variance explained')
-ax.set_xlim(0, num_eig_worms)
-ax.set_ylim(0, 1)
-ax.set_title('Variance explained by eigenworms')
+    eigenworms = np.load(os.path.join(path, r'all_eigenworms\eigen_worms_'+str(index)+'.npy'))
+    eigenworms_sum += eigenworms
+    # print(eigenworms_sum[3, :])
+    for i in range(num_eigenworms):
+        axs[i].plot(eigenworms[i, :], color='black', linewidth=1, linestyle='dashed')
+        # axs[i].set_xlim(0, numPixels)
+        # axs[i].set_ylim(0, eigenWorms)
 
-# Plot the eigenworms
-fig, axs = plt.subplots(nrows=num_eig_worms, ncols=1, figsize=(5, 10))
-for i in range(num_eig_worms):
-    axs[i].plot(eigen_worms[i, :], color=[1, 0.5, 0.1], linewidth=2)
+    index += 1
+
+for i in range(num_eigenworms):
+    axs[i].plot(eigenworms_sum[i, :]/(index-1), color='red', linewidth=1)
     # axs[i].set_xlim(0, numPixels)
     # axs[i].set_ylim(0, eigenWorms)
+
+# print(eigenworms_sum[3, :]/(index-1))
 
 plt.show()
